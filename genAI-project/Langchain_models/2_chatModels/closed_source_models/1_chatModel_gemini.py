9e85f31ebfadb1d2065f1ea3c2fd090811d5c990
MA-30 <ma-30@MA-30s-MacBook-Air.local>
2026-04-06 15:28:23 +0530

    -  Add initial implementations for chat models using Langchain and dotenv.

--- genAI-project/Langchain_models/2_chatModels/closed_source_models/1_chatModel_gemini.py
+++ genAI-project/Langchain_models/2_chatModels/closed_source_models/1_chatModel_gemini.py
@@ -1,15 +1,18 @@
+
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from openai import chat
+import os
+
 load_dotenv()
 
 # Temparature is a parameter that controls the randomness of the model's output. A higher temperature will result in more random and creative responses, while a lower temperature will produce more focused and deterministic responses. Temperature value range for gemini = 0.0 to 1.0
 # Max tokens is a parameter that limits the maximum number of tokens (words or subwords) in the generated response. Setting a lower max tokens value can help to keep the response concise and prevent it from being too long.
 
 chatModel1 = ChatGoogleGenerativeAI(
-    model = 'gemini-1.5-flash', 
+    model = 'gemini-2.5-flash', 
     temperature=0.9, 
-    max_tokens=10
+    # max_tokens=10,
+    api_key=os.getenv("GOOGLE_API_KEY")
     )
 try:
     res = chatModel1.invoke("Write a short poem on Lucifer, the fallen angel.")
